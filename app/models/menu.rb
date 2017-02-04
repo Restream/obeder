e57ce6f1f6e2939a15be61cf8be365108f8d4649
Svetlana Saveleva <ssaweleva@gmail.com>
2017-02-04 20:06:58 +0400

    fix

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,6 +3,7 @@ class Menu < ApplicationRecord
   has_many :dishes, through: :menu_dishes
   has_many :user_menus
   has_many :users, through: :user_menus
+
   accepts_nested_attributes_for :menu_dishes, reject_if: :all_blank, allow_destroy: true
 
   scope :for_week, -> { where('date >= ? AND date < ?', Date.today, Date.today.end_of_week) }
