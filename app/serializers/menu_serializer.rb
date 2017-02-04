2ea8f704346a48e5c722497a82da6854f82c9d4e
Svetlana Saveleva <ssaweleva@gmail.com>
2017-02-04 18:57:53 +0400

    add default flag to serializers

--- app/serializers/menu_serializer.rb
+++ app/serializers/menu_serializer.rb
@@ -1,3 +1,13 @@
 class MenuSerializer < ActiveModel::Serializer
   attributes :id, :date, :dishes
+
+  def dishes
+    object.menu_dishes.map do |menu_dish|
+      dish = menu_dish.dish
+      dish_hash = DishSerializer.new(dish).attributes
+      dish_hash[:default] = menu_dish.default
+
+      dish_hash
+    end
+  end
 end
--- app/serializers/user_menu_serializer.rb
+++ app/serializers/user_menu_serializer.rb
@@ -8,9 +8,11 @@ class UserMenuSerializer < ActiveModel::Serializer
 
     user_dish_ids = user_menu.dishes.pluck(:id)
 
-    object.dishes.map do |dish|
+    object.menu_dishes.map do |menu_dish|
+      dish = menu_dish.dish
       dish_hash = DishSerializer.new(dish).attributes
       dish_hash[:selected] = user_dish_ids.include?(dish.id)
+      dish_hash[:default] = menu_dish.default
 
       dish_hash
     end
