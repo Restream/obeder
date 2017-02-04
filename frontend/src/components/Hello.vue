46b221d3b5e783e05e14cdc356abf14eec7c9590
Pavel Beketov <beketov@restream.rt.ru>
2017-02-04 13:00:50 +0400

    Base frontend

---
+++ frontend/src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Zdarova',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
