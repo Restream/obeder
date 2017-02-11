679f5acf8ec83ca8e46ae1993964f2227dc37821
Alexander Andreev <alexander.andreev@restream.rt.ru>
2017-02-11 19:20:44 +0400

    fix n+1 on main

--- app/controllers/api/users/menus_controller.rb
+++ app/controllers/api/users/menus_controller.rb
@@ -1,6 +1,6 @@
 class Api::Users::MenusController < Api::Users::ApplicationController
   def index
-    user_menus = resource_user.user_menus.for_week
+    user_menus = resource_user.user_menus.includes(menu: [{ menu_dishes: [:dish] }]).for_week
     render json: user_menus
   end
 end
