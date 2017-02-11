577058ecf5af0a18d7dea1611ffcf015eba48cb9
Alexander Andreev <alexander.andreev@restream.rt.ru>
2017-02-11 17:59:17 +0400

    fix n+1

--- app/controllers/web/admin/user_menus_controller.rb
+++ app/controllers/web/admin/user_menus_controller.rb
@@ -2,7 +2,7 @@ class Web::Admin::UserMenusController < Web::Admin::ApplicationController
   def index
     @date = date
     @neem_users = User.where(neem: true)
-    @user_menus = UserMenu.em.for_date(params[:date])
+    @user_menus = UserMenu.with_users.with_dishes.em.for_date(params[:date]).by_user_name
     @dishes_stats = @user_menus.map(&:dishes).flatten.group_by(&:name)
       .map{ |key, value| { type: key, count: value.count } }
   end
--- app/models/user_menu.rb
+++ app/models/user_menu.rb
@@ -8,4 +8,7 @@ class UserMenu < ApplicationRecord
   scope :em, -> { where(user: User.where(neem: false)) }
   scope :for_date, ->(date) { where(menu: Menu.where(date: date)) }
   scope :for_week, -> { where(menu: Menu.where('date >= ? AND date < ?', Date.current, Date.current + 7.days)) }
+  scope :by_user_name, -> { order('users.name') }
+  scope :with_users, -> { includes(:user) }
+  scope :with_dishes, -> { includes(:dishes) }
 end
