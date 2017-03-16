c192cbd75b19aaae49008e85846ae7f41ec4bc32
Konkov Mikhail <konkov@restream.rt.ru>
2017-03-16 13:29:26 +0400

    add email validation

--- app/controllers/web/admin/users_controller.rb
+++ app/controllers/web/admin/users_controller.rb
@@ -5,8 +5,43 @@ class Web::Admin::UsersController < Web::Admin::ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update user_params
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :neem, :description)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, restream_email: true
+
   has_many :user_menus
   has_many :menus, through: :user_menus
 end
