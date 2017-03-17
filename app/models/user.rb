e712d071506e06f165129efef92c086edb4a612d
Konkov Mikhail <konkov@restream.rt.ru>
2017-03-17 09:01:01 +0400

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true, restream_email: true
-
   has_many :user_menus, dependent: :destroy
   has_many :menus, through: :user_menus
+
+  validates :name, presence: true
+  validates :email, presence: true, restream_email: true
 end
