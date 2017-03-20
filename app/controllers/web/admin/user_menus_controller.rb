f7cd9cf3d2f0cdd1ab8120ec350d2c056a2e0a2f
Igor Tulmentyev <igtulm@gmail.com>
2017-03-21 00:15:21 +0400

    em today shows in em list if global neem is checked

--- app/controllers/web/admin/user_menus_controller.rb
+++ app/controllers/web/admin/user_menus_controller.rb
@@ -2,8 +2,11 @@ class Web::Admin::UserMenusController < Web::Admin::ApplicationController
 
   def index
     @date = Date.parse(params[:date]) if params[:date]
-    @neem_users = User.joins(user_menus: :menu).where(neem: true).or(User.joins(user_menus: :menu).where('user_menus.neem = ? AND menus.date = ?', true, params[:date]))
-    @user_menus = UserMenu.with_users.with_dishes.em.for_date(params[:date]).by_user_name
+    @neem_users = User.joins(user_menus: :menu)
+                      .where(neem: true)
+                      .or(User.joins(user_menus: :menu).where('user_menus.neem = ? AND menus.date = ?', true, params[:date]))
+                      .distinct
+    @user_menus = UserMenu.with_users.em.with_dishes.em.for_date(params[:date]).where('users.neem = ?', true).by_user_name
     @dishes_stats = @user_menus.map(&:dishes).flatten.group_by(&:name)
       .map{ |key, value| { type: key, count: value.count } }
   end
