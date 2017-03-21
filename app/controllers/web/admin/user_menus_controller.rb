66c29a3c6ccb4213b04ad40c469d545831540c11
Igor Tulmentyev <igtulm@gmail.com>
2017-03-21 18:25:26 +0400

    dry method

--- app/controllers/web/admin/user_menus_controller.rb
+++ app/controllers/web/admin/user_menus_controller.rb
@@ -1,12 +1,13 @@
 class Web::Admin::UserMenusController < Web::Admin::ApplicationController
 
   def index
-    @date = Date.parse(params[:date]) if params[:date]
+    date = params[:date]
+    @date = Date.parse(date) if date
     @neem_users = User.joins(user_menus: :menu)
                       .where(neem: true)
-                      .or(User.joins(user_menus: :menu).where('user_menus.neem = ? AND menus.date = ?', true, params[:date]))
+                      .or(User.joins(user_menus: :menu).where('user_menus.neem = ? AND menus.date = ?', true, date))
                       .distinct
-    @user_menus = UserMenu.with_users.with_dishes.em.for_date(params[:date])
+    @user_menus = UserMenu.with_users.with_dishes.em.for_date(date)
                           .where('users.neem = ? AND user_menus.neem = ?', false, false)
                           .by_user_name
     @dishes_stats = @user_menus.map(&:dishes).flatten.group_by(&:name)
