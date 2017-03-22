a630ed0aec515fb2c5d1c5f69e928231a68bdf21
mkalmykov <m.n.kalmykov@gmail.com>
2017-03-22 18:37:14 +0400

    #14 Add main and separate dishes number validation

--- app/validators/menu_publish_readiness_validator.rb
+++ app/validators/menu_publish_readiness_validator.rb
@@ -1,25 +1,33 @@
 class MenuPublishReadinessValidator < ActiveModel::Validator
   def validate(record)
     errors = record.errors
-    errors.add(:dishes, :less_than_two_soups) unless more_than?(record, :soup, 2)
-    errors.add(:dishes, :less_than_two_salads) unless more_than?(record, :salad, 2)
-    has_defaults = (exists_default?(record, :soup) && exists_default?(record, :salad)) ||
-                   (exists_default?(record, :main_dish) && exists_default?(record, :side_dish)) ||
-                    exists_default?(record, :separate_dish)
+    dishes = record.dishes
+    errors.add(:dishes, :less_than_two_soups) unless more_than?(dishes, :soup, 2)
+    errors.add(:dishes, :less_than_two_salads) unless more_than?(dishes, :salad, 2)
+
+    main_and_separate_dishes = dishes.where(dish_type: :main_dish)
+                              .or(dishes.where(dish_type: :main_dish))
+                              .size
+
+    errors.add(:dishes, :less_than_two_main_dishes) unless main_and_separate_dishes > 2
+
+    has_defaults = (exists_default?(dishes, :soup) && exists_default?(dishes, :salad)) ||
+                   (exists_default?(dishes, :main_dish) && exists_default?(dishes, :side_dish)) ||
+                    exists_default?(dishes, :separate_dish)
 
     errors.add(:dishes, :no_defaults) unless has_defaults
-    errors.add(:dishes, :main_to_side_dishes_mismatch) unless quantity_matches?(record, :main_dish, :side_dish)
+    errors.add(:dishes, :main_to_side_dishes_mismatch) unless quantity_matches?(dishes, :main_dish, :side_dish)
   end
 
-  def more_than?(record, dish_type, required_number)
-    record.dishes.where(dish_type: dish_type).size > required_number
+  def more_than?(dishes, dish_type, required_number)
+    dishes.where(dish_type: dish_type).size > required_number
   end
 
-  def exists_default?(record, dish_type)
-    record.dishes.where(dish_type: dish_type).any? { |dish| dish.menu_dishes.exists?(default: true) }
+  def exists_default?(dishes, dish_type)
+    dishes.where(dish_type: dish_type).any? { |dish| dish.menu_dishes.exists?(default: true) }
   end
 
-  def quantity_matches?(record, dish_one, dish_two)
-    record.dishes.where(dish_type: dish_one).size == record.dishes.where(dish_type: dish_two).size
+  def quantity_matches?(dishes, dish_one, dish_two)
+    dishes.where(dish_type: dish_one).size == dishes.where(dish_type: dish_two).size
   end
 end
