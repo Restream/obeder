8d9586013835c8be9b0abd9994c938985af567d6
Igor Tulmentyev <igtulm@gmail.com>
2017-03-22 13:52:18 +0400

    query refactoring

--- app/controllers/web/admin/user_menus_controller.rb
+++ app/controllers/web/admin/user_menus_controller.rb
@@ -8,8 +8,7 @@ class Web::Admin::UserMenusController < Web::Admin::ApplicationController
       .or(User.joins(user_menus: :menu).where('user_menus.neem = ? AND menus.date = ?', true, date))
       .distinct
     @user_menus = UserMenu.with_users.with_dishes.em.for_date(date)
-      .where('users.neem = ? AND user_menus.neem = ?', false, false)
-      .by_user_name
+      .where.not(user: @neem_users).by_user_name
     @dishes_stats = @user_menus.map(&:dishes).flatten.group_by(&:name)
       .map{ |key, value| { type: key, count: value.count } }
   end
