c33ed4252afdb4a5a1099e214fc4bf5ca0727fab
Igor Tulmentyev <igtulm@gmail.com>
2017-03-22 12:54:48 +0400

    change indentation

--- app/controllers/web/admin/user_menus_controller.rb
+++ app/controllers/web/admin/user_menus_controller.rb
@@ -4,12 +4,12 @@ class Web::Admin::UserMenusController < Web::Admin::ApplicationController
     date = params[:date]
     @date = Date.parse(date) if date
     @neem_users = User.joins(user_menus: :menu)
-                      .where(neem: true)
-                      .or(User.joins(user_menus: :menu).where('user_menus.neem = ? AND menus.date = ?', true, date))
-                      .distinct
+      .where(neem: true)
+      .or(User.joins(user_menus: :menu).where('user_menus.neem = ? AND menus.date = ?', true, date))
+      .distinct
     @user_menus = UserMenu.with_users.with_dishes.em.for_date(date)
-                          .where('users.neem = ? AND user_menus.neem = ?', false, false)
-                          .by_user_name
+      .where('users.neem = ? AND user_menus.neem = ?', false, false)
+      .by_user_name
     @dishes_stats = @user_menus.map(&:dishes).flatten.group_by(&:name)
       .map{ |key, value| { type: key, count: value.count } }
   end
