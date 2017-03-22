f70dc1893306c6e5a0fa856af611fcf06ab71545
Igor Tulmentyev <igtulm@gmail.com>
2017-03-22 12:21:35 +0400

    remove unused styles

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -101,66 +101,6 @@
     height: 24px;
   }
 
-  .header__switcher {
-    display: flex;
-    align-items: center;
-  }
-
-  .header__switcher-label {
-    font-weight: 400;
-    font-size: 20px;
-    color: #4D4D4D;
-    letter-spacing: 0.01px;
-    display: inline-block;
-    margin: 0 14px;
-  }
-
-  .label {
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    height: 26px;
-
-    & input {
-      display: none;
-    }
-
-    & input:checked {
-      & + .circle {
-        &::after {
-          transform: translateX(24px);
-          background: #38B5C7;
-          border: none;
-        }
-      }
-    }
-  }
-
-  .circle {
-    background: rgba(0,0,0,0.30);
-    height: 2px;
-    width: 50px;
-    position: relative;
-    top: 2px;
-    display: inline-block;
-    font-size: 0;
-
-    &::after {
-      content: '';
-      display: block;
-      width: 26px;
-      height: 26px;
-      position: absolute;
-      left: 0;
-      top: -13px;
-      background-color: #fff;
-      border: 2px solid #4a4a4a;
-      border-radius: 50%;
-      transition: transform 300ms ease-in-out;
-      box-sizing: border-box;
-    }
-  }
-
   @media (--desktop) {
     .header__logo {
       display: flex;
