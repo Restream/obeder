f7a8169be6ae151deba728d42907cdace1f1bcba
mkalmykov <m.n.kalmykov@gmail.com>
2017-03-22 16:30:09 +0400

    #14 Добавлены проверки в валидатор

--- app/validators/menu_publish_readiness_validator.rb
+++ app/validators/menu_publish_readiness_validator.rb
@@ -1,6 +1,26 @@
 class MenuPublishReadinessValidator < ActiveModel::Validator
   def validate(record)
-    record.errors[:dishes] << 'Менее двух супов' unless record.dishes.where(dish_type: :soup).size < 2
-    record.errors[:dishes] << 'Менее двух салатов' unless record.dishes.where(dish_type: :salad).size < 2
+    errors = record.errors[:dishes]
+    errors << 'Менее двух супов' unless less_than?(record, :soup, 2)
+    errors << 'Менее двух салатов' unless less_than?(record, :salad, 2)
+
+    has_defaults = (exists_default?(record, :soup) && exists_default?(record, :salad)) ||
+                   (exists_default?(record, :main_dish) && exists_default?(record, :side_dish)) ||
+                    exists_default?(record, :separate_dish)
+
+    errors << 'Не указаны блюда по умолчанию' unless has_defaults
+    errors << 'Количество гарниров не совпадает с количеством основных блюд' unless quantity_matches?(record, :main_dish, :side_dish)
+  end
+
+  def less_than?(record, dish_type, required_number)
+    record.dishes.where(dish_type: dish_type).size < required_number
+  end
+
+  def exists_default?(record, dish_type)
+    record.dishes.where(dish_type: dish_type).menu_dishes.exists?(default: true)
+  end
+
+  def quantity_matches?(record, dish_one, dish_two)
+    record.dishes.where(dish_type: dish_one).size == record.dishes.where(dish_type: dish_two).size
   end
 end
