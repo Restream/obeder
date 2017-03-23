39a59b5b714dc71a79e64574dca9a18d50ce8376
mkalmykov <m.n.kalmykov@gmail.com>
2017-03-23 16:04:28 +0400

    #14 Validator refactoring

--- app/validators/menu_publish_readiness_validator.rb
+++ app/validators/menu_publish_readiness_validator.rb
@@ -2,25 +2,17 @@ class MenuPublishReadinessValidator < ActiveModel::Validator
   def validate(record)
     errors = record.errors
     dishes = record.dishes
-    errors.add(:dishes, :less_than_two_soups) unless more_than?(dishes, :soup, 1)
-    errors.add(:dishes, :less_than_two_salads) unless more_than?(dishes, :salad, 1)
-
-    main_and_separate_dishes = dishes.where(dish_type: :main_dish)
-                              .or(dishes.where(dish_type: :separate_dish))
-                              .size
-
-    errors.add(:dishes, :less_than_two_main_dishes) unless main_and_separate_dishes > 1
-
-    has_defaults = (exists_default?(dishes, :soup) && exists_default?(dishes, :salad)) ||
-                   (exists_default?(dishes, :main_dish) && exists_default?(dishes, :side_dish)) ||
-                    exists_default?(dishes, :separate_dish)
-
-    errors.add(:dishes, :no_defaults) unless has_defaults
+    errors.add(:dishes, :less_than_two_soups) if less_than?(dishes, :soup, 2)
+    errors.add(:dishes, :less_than_two_salads) if less_than?(dishes, :salad, 2)
+    errors.add(:dishes, :less_than_two_main_dishes) if main_and_separate_dishes_quantity(dishes) < 2
+    errors.add(:dishes, :no_defaults) unless has_defaults?(dishes)
     errors.add(:dishes, :main_to_side_dishes_mismatch) unless quantity_matches?(dishes, :main_dish, :side_dish)
   end
 
-  def more_than?(dishes, dish_type, required_number)
-    dishes.where(dish_type: dish_type).size > required_number
+  private
+
+  def less_than?(dishes, dish_type, required_number)
+    dishes.where(dish_type: dish_type).size < required_number
   end
 
   def exists_default?(dishes, dish_type)
@@ -29,5 +21,17 @@ class MenuPublishReadinessValidator < ActiveModel::Validator
 
   def quantity_matches?(dishes, dish_one, dish_two)
     dishes.where(dish_type: dish_one).size == dishes.where(dish_type: dish_two).size
+  end
+
+  def main_and_separate_dishes_quantity(dishes)
+    dishes.where(dish_type: :main_dish)
+      .or(dishes.where(dish_type: :separate_dish))
+      .size
+  end
+
+  def has_defaults?(dishes)
+    (exists_default?(dishes, :soup) && exists_default?(dishes, :salad)) ||
+    (exists_default?(dishes, :main_dish) && exists_default?(dishes, :side_dish)) ||
+    exists_default?(dishes, :separate_dish)
   end
 end
