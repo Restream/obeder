3b8a21a1dcb386015ff9db1357b6de2e4c2b8d60
mkalmykov <m.n.kalmykov@gmail.com>
2017-03-23 13:29:05 +0400

    #14 Finished validation pop-up

--- app/assets/javascripts/web/admin/menus/confirm_publish.js
+++ app/assets/javascripts/web/admin/menus/confirm_publish.js
@@ -1,14 +1,40 @@
 (function() {
-  var publishForm = $('#publish_form');
   var publishBtn = $('#publish_btn');
-  var validationUrl = publishForm.attr('validationUrl');
+  var publishModal = $('#publish_modal');
+  var publishSubmitBtn = $('#publish_submit_btn')
+  var validationUrl = publishModal.attr('validationUrl');
 
-  console.log(validationUrl);
+  var validationMarkers = {
+    inProgress: $('#in_validation_marker'),
+    ok: $('#validation_ok_marker'),
+    failed: $('#validation_failed_marker')
+  };
+
+  var validationErrorsList = $('#validation_errors');
+
+  publishModal.on('show.bs.modal', function (e) {
+    validationMarkers.inProgress.show();
+    validationMarkers.ok.hide();
+    validationMarkers.failed.hide();
+  });
+
+  publishModal.on('hide.bs.modal', function (e) {
+    publishSubmitBtn.prop('disabled', true);;
+  });
 
   publishBtn.click(function(e) {
     $.get(validationUrl, function(data) {
-      console.log(data);
+      validationMarkers.inProgress.hide();
+
+      if (!data.valid) {
+        validationMarkers.failed.show();
+        var errors = '<li>' + data.errors.join('</li><li>') + '</li>';
+        validationErrorsList.html(errors);
+      } else {
+        validationMarkers.ok.show();
+      }
+
+      publishSubmitBtn.prop('disabled', false);
     });
-    e.preventDefault();
   });
 })()
