ddb25f87e53eab8da61541b52252d866a5552f04
mkalmykov <m.n.kalmykov@gmail.com>
2017-03-24 10:03:44 +0400

    #14 Codestyle fixes

--- app/validators/menu_publish_readiness_validator.rb
+++ app/validators/menu_publish_readiness_validator.rb
@@ -4,7 +4,7 @@ class MenuPublishReadinessValidator < ActiveModel::Validator
     dishes = record.dishes
     errors.add(:dishes, :less_than_two_soups) if less_than?(dishes, :soup, 2)
     errors.add(:dishes, :less_than_two_salads) if less_than?(dishes, :salad, 2)
-    errors.add(:dishes, :less_than_two_main_dishes) if main_and_separate_dishes_quantity(dishes) < 2
+    errors.add(:dishes, :less_than_two_main_dishes) if main_and_separate_dishes_count(dishes) < 2
     errors.add(:dishes, :no_defaults) unless has_defaults?(dishes)
     errors.add(:dishes, :main_to_side_dishes_mismatch) unless quantity_matches?(dishes, :main_dish, :side_dish)
   end
@@ -23,7 +23,7 @@ class MenuPublishReadinessValidator < ActiveModel::Validator
     dishes.where(dish_type: dish_one).size == dishes.where(dish_type: dish_two).size
   end
 
-  def main_and_separate_dishes_quantity(dishes)
+  def main_and_separate_dishes_count(dishes)
     dishes.where(dish_type: :main_dish)
       .or(dishes.where(dish_type: :separate_dish))
       .size
