a66b0745b8f0609dd7650b9aa9b69d9098289d7a
mkalmykov <m.n.kalmykov@gmail.com>
2017-03-24 15:57:50 +0400

    #14 Add test

--- test/controllers/api/admin/menus_controller_test.rb
+++ test/controllers/api/admin/menus_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
-class Api::Admin::MenusControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class Api::Admin::MenusControllerTest < ActionController::TestCase
+  setup do
+    admin_http_login
+
+    @menu = create :menu
+  end
+
+  test 'validate' do
+    get :validate, params: { date: @menu.date.to_s }
+    assert_response :success
+  end
 end
