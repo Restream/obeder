9eeaa141216aa5fe426de93b3f70b3d3a29211fe
Konkov Mikhail <konkov@restream.rt.ru>
2017-03-27 17:36:56 +0400

    add validation email for cook

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   has_many :user_menus, dependent: :destroy
   has_many :menus, through: :user_menus
 
-  validates :name, presence: true
-  validates :role, presence: true
-  validates :email, presence: true, restream_email: true
+  validates :name, :role, :email, presence: true
+  validates :email, email: true, if: "role.cook?"
+  validates :email, restream_email: true, unless: "role.cook?"
   validates_confirmation_of :password
 
   has_secure_password validations: false
--- app/validators/restream_email_validator.rb
+++ app/validators/restream_email_validator.rb
@@ -1,7 +1,7 @@
 class RestreamEmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@restream.rt.ru\z/i
-      record.errors.add(attribute, :invalid_email)
+      record.errors.add(attribute, :invalid_restream_email)
     end
   end
 end
