642f616e9d567b9c214b757889eee82b0dd0cb00
Konkov Mikhail <konkov@restream.rt.ru>
2017-03-27 15:16:54 +0400

    remove users with cook role from reports

--- app/controllers/web/admin/user_menus_controller.rb
+++ app/controllers/web/admin/user_menus_controller.rb
@@ -4,12 +4,16 @@ class Web::Admin::UserMenusController < Web::Admin::ApplicationController
   def index
     date = params[:date]
     @date = Date.parse(date) if date
+
     @neem_users = User.joins(user_menus: :menu)
-      .where(neem: true)
-      .or(User.joins(user_menus: :menu).where('user_menus.neem = ? AND menus.date = ?', true, date))
+      .where('users.neem = ? OR (user_menus.neem = ? AND menus.date = ?)', true, true, date)
+      .where.not(role: 'cook')
       .distinct
+
     @user_menus = UserMenu.with_users.with_dishes.for_date(date)
-      .where.not(user: @neem_users).by_user_name
+      .where.not(user: @neem_users, users: { role: 'cook' })
+      .ordered_by_user_name
+
     @dishes_stats = @user_menus.map(&:dishes).flatten.group_by(&:name)
       .map{ |key, value| { type: key, count: value.count } }
   end
