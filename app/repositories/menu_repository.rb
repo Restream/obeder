1b7d6d3938aa9a3afb6985dc7b5f1ba71ac7f583
mkalmykov <m.n.kalmykov@gmail.com>
2017-03-27 14:08:52 +0400

    #14 Simplifying current_menu method

--- app/repositories/menu_repository.rb
+++ app/repositories/menu_repository.rb
@@ -5,7 +5,7 @@ module MenuRepository
   
   included do
     scope :for_date, -> (date) { where(date: date) }
-    scope :current_menu, -> (date) { for_date(date).first || create(date: date) }
+    scope :current_menu, -> (date) { find_or_create_by(date: date) }
     scope :for_date_range, -> (date, offset) { where(date: (date - offset.days)..(date + offset.days)).order(:date) }
     scope :except_date, -> (date) { where.not(date: date) }
     scope :ready, -> { where(ready: true) }
