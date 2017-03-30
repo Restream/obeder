2f28f023035030ca678a912476eab9eb4206164f
mkalmykov <m.n.kalmykov@gmail.com>
2017-03-30 14:44:02 +0400

    #24 Add class definition in migration, add new hash syntax

--- db/migrate/20170328131232_add_unique_index_to_user_menu_dishes.rb
+++ db/migrate/20170328131232_add_unique_index_to_user_menu_dishes.rb
@@ -1,3 +1,5 @@
+class UserMenuDish < ActiveRecord::Base; end
+
 class AddUniqueIndexToUserMenuDishes < ActiveRecord::Migration[5.0]
   def change
     grouped = UserMenuDish.all.group_by{ |model| [model.user_menu_id, model.dish_id] }
@@ -6,6 +8,6 @@ class AddUniqueIndexToUserMenuDishes < ActiveRecord::Migration[5.0]
       duplicates.each{ |duplicate| duplicate.destroy }
     end
 
-    add_index :user_menu_dishes, [:user_menu_id, :dish_id], :unique => true
+    add_index :user_menu_dishes, [:user_menu_id, :dish_id], unique: true
   end
 end
