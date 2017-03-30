2cad8b2bb931b241ddee95eb2810edc9b9f2bea9
Konkov Mikhail <konkov@restream.rt.ru>
2017-03-30 15:40:17 +0400

    fix after refactor

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
       end
     end
     resource :session, only: [:new, :create, :destroy]
-    get :login, to: :new, controller: :sessions
-    get :logout, to: :destroy, controller: :sessions
+    get :login, to: 'sessions#new'
+    get :logout, to: 'sessions#destroy'
 
     namespace :admin do
       root to: 'welcome#index'
