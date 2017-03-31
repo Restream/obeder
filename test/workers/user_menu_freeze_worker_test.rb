d350eb7c467fde0a5ac09261dab57e336932f3ea
Igor Tulmentyev <igtulm@gmail.com>
2017-03-31 19:02:48 +0400

    Add test for worker

--- test/workers/user_menu_freeze_worker_test.rb
+++ test/workers/user_menu_freeze_worker_test.rb
@@ -1,6 +1,28 @@
 require 'test_helper'
-class GuzzleWorkerTest < MiniTest::Unit::TestCase
-  def test_example
-    skip "add some examples to (or delete) #{__FILE__}"
+
+class UserMenuFreezeWorkerTest < ActiveSupport::TestCase
+  setup do
+    @em_users = create_list :user, 5, neem: false
+    @neem_users = create_list :user, 5, neem: true
+    menu = create :menu, date: Date.tomorrow
+    @em_users.each { |user| create :user_menu, user: user, menu: menu, neem: false }
+    @neem_users.each { |user| create :user_menu, user: user, menu: menu, neem: false }
+  end
+
+  test 'user_menus_freeze' do
+    worker = UserMenuFreezeWorker.new
+    worker.perform
+
+    @em_users.each do |user|
+      user.reload
+      assert user.user_menus.where(neem: false).exists?
+      assert user.user_menus.where(neem: true).empty?
+    end
+
+    @neem_users.each do |user|
+      user.reload
+      assert user.user_menus.where(neem: true).exists?
+      assert user.user_menus.where(neem: false).empty?
+    end
   end
 end
