358fc9906b04e0e08bd7542026bf8df39d135357
Konkov Mikhail <konkov@restream.rt.ru>
2017-04-03 11:59:58 +0400

    add remind password

---
+++ app/types/user_remind_password_type.rb
@@ -0,0 +1,7 @@
+class UserRemindPasswordType < ActiveType::Object
+  attribute :email, :string
+  validates :email, presence: true, email: true
+  def user
+    User.find_by(email: email.mb_chars.downcase)
+  end
+end
