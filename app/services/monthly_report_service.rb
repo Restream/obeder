82251f953303cafd0e3964f6133df1d7e6e25681
Igor Tulmentyev <igtulm@gmail.com>
2017-04-07 03:03:30 +0400

    #29 some xlsx content

--- app/services/monthly_report_service.rb
+++ app/services/monthly_report_service.rb
@@ -1,7 +1,7 @@
 class MonthlyReportService
   def initialize(month, year)
-    @month = month
-    @year = year
+    @month = month.to_i
+    @year = year.to_i
   end
 
   def export_to_xlsx_stream
@@ -9,7 +9,7 @@ class MonthlyReportService
 
     workbook = package.workbook
     workbook.add_worksheet(name: name) do |sheet|
-
+      sheet.add_row(xlsx_table_header)
     end
 
     { read: package.to_stream.read, filename: "#{name}.xlsx" , type: 'application/vnd.ms-excel' }
@@ -19,5 +19,21 @@ class MonthlyReportService
 
   def name
     "#{@month}_#{@year}"
+  end
+
+  def array_of_days
+    (1..Time.days_in_month(@month, @year)).to_a.map(&:to_s)
+  end
+
+  def i18n_name_column
+    I18n.t('web.admin.reports.index.name_column_title')
+  end
+
+  def sum_symb
+    '∑'
+  end
+
+  def xlsx_table_header
+    ([] << i18n_name_column) + array_of_days << sum_symb
   end
 end
