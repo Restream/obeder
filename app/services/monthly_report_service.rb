d56744e946ec8d395d2802ffd36ee81c45495664
Igor Tulmentyev <igtulm@gmail.com>
2017-04-11 00:02:27 +0400

    refactoring

--- app/services/monthly_report_service.rb
+++ app/services/monthly_report_service.rb
@@ -42,16 +42,14 @@ class MonthlyReportService
       sheet.add_row(report_upload_date, style: workbook_styles[:uploaded])
       sheet.add_row()
 
-      header_styles = table_header_styles.map { |style| workbook_styles[style] }
-      sheet.add_row(report_table_header, style: header_styles)
+      sheet.add_row(report_table_header,
+        style: table_header_styles.map { |style| workbook_styles[style] })
       @users.each do |user|
-        user_info_row = report_table_user_info(user)
-        user_styles = table_user_styles.map { |style| workbook_styles[style] }
-        sheet.add_row(user_info_row, style: user_styles)
+        sheet.add_row(report_table_user_info(user),
+          style: table_user_styles.map { |style| workbook_styles[style] })
       end
 
-      styles = total_styles.map { |style| workbook_styles[style] }
-      sheet.add_row(report_table_total, style: styles)
+      sheet.add_row(report_table_total, style: total_styles.map { |style| workbook_styles[style] })
     end
 
     package.to_stream.read
