6586f579e98f29389e9780b867d72cb37398acdf
mkalmykov <m.n.kalmykov@gmail.com>
2017-04-13 12:36:45 +0400

    Fix sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}/#{ENV["REDIS_DB"]}"
+    url: "redis://#{ENV.fetch("REDIS_HOST") {"127.0.0.1"}}:#{ENV.fetch("REDIS_PORT") {"6379"}}/#{ENV.fetch("REDIS_DB") {"db0"}}"
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}/#{ENV["REDIS_DB"]}"
+    url: "redis://#{ENV.fetch("REDIS_HOST") {"127.0.0.1"}}:#{ENV.fetch("REDIS_PORT") {"6379"}}/#{ENV.fetch("REDIS_DB") {"db0"}}"
   }
 end
