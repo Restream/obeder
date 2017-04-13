4145fab5f59f833244983e424aed8a16790624e2
mkalmykov <m.n.kalmykov@gmail.com>
2017-04-13 16:58:04 +0400

    Remove homedir

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,6 @@ set :branch, :master
 set :rails_env, :production
 
 set :user, 'root'
-set :homdir, '/root'
 set :ssh_options, user: fetch(:user)
 
-set :deploy_to, "#{fetch(:homdir)}/apps/#{fetch(:application)}"
+set :deploy_to, "/root/apps/#{fetch(:application)}"
