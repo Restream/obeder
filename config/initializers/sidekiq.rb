2472f413152c12f4588300451da1df563e628a31
mkalmykov <m.n.kalmykov@gmail.com>
2017-04-13 17:15:46 +0400

    Redis configs to settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: "redis://#{ENV.fetch("REDIS_HOST") { "127.0.0.1" }}:#{ENV.fetch("REDIS_PORT") { "6379" }}/#{ENV.fetch("REDIS_DB") { "db0" }}"
+    url: "redis://#{ENV.fetch("REDIS_HOST", Settings.REDIS_HOST)}:#{ENV.fetch("REDIS_PORT", Settings.REDIS_PORT)}/#{ENV.fetch("REDIS_DB", Settings.SIDEKIQ_REDIS_DB)}"
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: "redis://#{ENV.fetch("REDIS_HOST") { "127.0.0.1" }}:#{ENV.fetch("REDIS_PORT") { "6379" }}/#{ENV.fetch("REDIS_DB") { "db0" }}"
+    url: "redis://#{ENV.fetch("REDIS_HOST", Settings.REDIS_HOST)}:#{ENV.fetch("REDIS_PORT", Settings.REDIS_PORT)}/#{ENV.fetch("REDIS_DB", Settings.SIDEKIQ_REDIS_DB)}"
   }
 end
