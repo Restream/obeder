b64161b4ac449c53e4dc89551a9316ddd993ff49
mkalmykov <m.n.kalmykov@gmail.com>
2017-04-14 12:10:11 +0400

    #29 Move row building logic to helper

--- app/helpers/web/admin/monthly_reports_helper.rb
+++ app/helpers/web/admin/monthly_reports_helper.rb
@@ -22,4 +22,26 @@ module Web::Admin::MonthlyReportsHelper
   def user_ate?(user_id, date, user_menus_info)
     user_menus_info.include?([date, user_id])
   end
+
+  def report_row(user, user_index, dates, user_menus)
+    total_by_user = 0
+
+    name_cell = content_tag(:td) do
+      [user_index + 1, user.name].join('. ')
+    end
+
+    dates_collection = dates.collect do |date|
+      if user_ate?(user.id, date, user_menus)
+        total_by_user += 1
+        content_tag :td, 1, class: cell_color_class(date)
+      else
+        content_tag :td, '', class: cell_color_class(date)
+      end
+    end
+
+    dates_cells = dates_collection.join().html_safe
+
+    total_cell = content_tag :td, total_by_user
+    content_tag :tr, name_cell.concat(dates_cells).concat(total_cell), class: 'text-center'
+  end
 end
