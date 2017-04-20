c79677d94a07c2444ba475089c3c3af8efa14509
Svetlana Saveleva <ssaweleva@gmail.com>
2017-04-20 13:00:38 +0400

    on async perform

--- app/services/menus_service.rb
+++ app/services/menus_service.rb
@@ -1,11 +1,11 @@
 class MenusService
   class << self
     def create_user_menus(menu)
-      UserMenusCreateWorker.new.perform(menu.id)
+      UserMenusCreateWorker.perform_async(menu.id)
     end
 
     def notify_menu_published(menu)
-      UserMenusNotifyWorker.new.perform(menu.id)
+      UserMenusNotifyWorker.perform_async(menu.id)
     end
   end
 end
