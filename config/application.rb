3f4b00416d53f35617f0483b3d8caebcb0ca5194
nutsoriginal <nutsoriginal@gmail.com>
2018-02-09 14:01:01 +0400

    Set time zone in config

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Obeder
       g.test_framework :test_unit, fixture: false, fixture_replacement: :factory_girl
     end
 
+    config.time_zone = 'Samara'
     config.i18n.default_locale = :ru
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.active_record.observers = :menu_observer
