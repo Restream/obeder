cfc566b02023185975a1d540995db11fd5267eb0
nutsoriginal <nutsoriginal@gmail.com>
2018-02-26 12:32:26 +0400

    Set time zone for Clockwork

--- clock.rb
+++ clock.rb
@@ -1,10 +1,13 @@
 require './config/boot'
 require './config/environment'
-
 require 'clockwork'
 
-include Clockwork
+module Clockwork
+  configure do |config|
+    config[:tz] = Time.zone.name
+  end
 
-every(1.day, 'freeze_user_menu', at: Settings.freeze_user_menu_at) do
-  UserMenuFreezeWorker.perform_async
+  every(1.day, 'freeze_user_menu', at: Settings.freeze_user_menu_at) do
+    UserMenuFreezeWorker.perform_async
+  end
 end
