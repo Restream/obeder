09c565ac807bc5e462cb9c34464dcbf02e7f93b2
Andrey Bukanin <bukanin@restream.rt.ru>
2019-03-26 12:59:40 +0400

    Changed corporate email validation be able to pass '@rt.ru' also

--- app/validators/restream_email_validator.rb
+++ app/validators/restream_email_validator.rb
@@ -1,6 +1,6 @@
 class RestreamEmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@restream.rt.ru\z/i
+    unless value =~ /\A([^@\s]+)@(restream.)?rt.ru\z/i
       record.errors.add(attribute, :invalid_restream_email)
     end
   end
